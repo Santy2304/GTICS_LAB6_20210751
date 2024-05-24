1fc8ea6562bce17f569a51a823dee6b631a4c6bf
Santy2304 <a20210751@pucp.edu.pe>
2024-05-23 19:30:09 -0500

    inicio del laboratorio

---
+++ src/test/java/com/example/lab6_gtics_20210751/Lab6Gtics20210751ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6_gtics_20210751;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6Gtics20210751ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
